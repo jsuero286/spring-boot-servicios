92106baaefdb4d9ae8500608d218ae3da7a7664b
xuxo <jesus.suero@gmail.com>
2020-04-22 13:02:53 +0200

    added logs

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -30,24 +30,25 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RefreshScope
 @RestController
 public class ItemController {
-	
+
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
-	
+
 	@Value("${configuracion.texto}")
 	private String texto;
-	
+
 	@GetMapping("/listar")
 	public List<Item> listar(){
+		log.info("test");
 		return itemService.findAll();
 	}
-	
+
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
@@ -57,45 +58,45 @@ public class ItemController {
 	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
-		
+
 		item.setCantidad(cantidad);
 		producto.setId(id);
 		producto.setNombre("Camara Sony");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 		return item;
-		
+
 	}
-	
+
 	@GetMapping("/obtener-config")
 	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
-		
+
 		log.info(texto);
-		
+
 		Map<String, String> json = new HashMap<>();
 		json.put("texto", texto);
 		json.put("puerto", puerto);
-		
+
 		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
 			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
 			json.put("autor.email", env.getProperty("configuracion.autor.email"));
 		}
-		
+
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/crear")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto crear(@RequestBody Producto producto) {
 		return itemService.save(producto);
 	}
-	
+
 	@PutMapping("/editar/{id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
 		return itemService.update(producto, id);
 	}
-	
+
 	@DeleteMapping("/eliminar/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void eliminar(@PathVariable Long id) {
